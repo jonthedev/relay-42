208b5fb0bdd168a66c36111fe14867e7b0859e1d
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-06-10 14:02:10 +0200

    chore: setup react router
    refactor: move App markup into Home page markup
    
    feat: react router serves home page on default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import {
-  addMission,
-  editMission,
-  selectMission,
-  selectMissions
-} from "@/features/missions/missionsSlice"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
 
-import { BtnPrimary } from "./components"
-import { mockMission } from "./store/tests/missions-spec/missionsMockData"
-import { useAppDispatch, useAppSelector } from "./store/storeHooks"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 
 function App() {
-  const { missions } = useAppSelector(selectMissions)
-  const mission = useAppSelector(state => selectMission(state, 1))
-  const dispatch = useAppDispatch()
-
-  return (
-    <>
-      <h1>Relay 42</h1>
-      <BtnPrimary />
-      {missions.map(mission => (
-        <p key={mission.id}>{mission.destination}</p>
-      ))}
-      <button onClick={() => dispatch(addMission(mockMission))}>
-        Add Mission
-      </button>
-      <button
-        onClick={() =>
-          dispatch(editMission({ ...mockMission, destination: "Earth", id: 1 }))
-        }
-      >
-        Edit Mission
-      </button>
-      Mission: {mission?.destination}
-    </>
-  )
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import {
+  addMission,
+  editMission,
+  selectMission,
+  selectMissions
+} from "@/features/missions/missionsSlice"
+
+import { BtnPrimary } from "@/components"
+import { mockMission } from "@/store/tests/missions-spec/missionsMockData"
+import { useAppDispatch, useAppSelector } from "@/store/storeHooks"
+
+const Home = () => {
+  const { missions } = useAppSelector(selectMissions)
+  const mission = useAppSelector(state => selectMission(state, 1))
+  const dispatch = useAppDispatch()
+  return (
+    <>
+      <h1>Relay 42</h1>
+      <BtnPrimary />
+      {missions.map(mission => (
+        <p key={mission.id}>{mission.destination}</p>
+      ))}
+      <button onClick={() => dispatch(addMission(mockMission))}>
+        Add Mission
+      </button>
+      <button
+        onClick={() =>
+          dispatch(editMission({ ...mockMission, destination: "Earth", id: 1 }))
+        }
+      >
+        Edit Mission
+      </button>
+      Mission: {mission?.destination}
+    </>
+  )
+}
+export default Home
