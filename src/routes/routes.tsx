67121643335eee27084d8c42930d28d3d45ba64b
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-06-11 18:07:29 +0200

    feat: add new-mission route to routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "@/pages/Home"
 import Error from "@/pages/Error"
+import MissionManagement from "@/pages/MissionManagement"
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <Error />
+  },
+  {
+    path: "/new-mission",
+    element: <MissionManagement />,
     errorElement: <Error />
   }
 ])
