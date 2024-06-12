105adf0b5b66e759b5f3e8ceaf82a674879633d9
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-06-12 23:09:20 +0200

    refactor: move mission related actions to missions pages
    chore: delete Home page

--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -1,7 +1,41 @@
-const Missions = ({ heading }: { heading: string }) => {
-  if (heading) {
-    return <h1>{heading}</h1>
-  }
-  return <div>Missions Page</div>
+import {
+  addMission,
+  editMission,
+  selectMission,
+  selectMissions
+} from "@/features/missions/missionsSlice"
+
+import { BtnPrimary } from "@/components"
+import {
+  mockMission,
+  mockMissionId1
+} from "@/tests/store/missions/missionsMockData"
+import { useAppDispatch, useAppSelector } from "@/store/storeHooks"
+
+const Missions = () => {
+  const { missions } = useAppSelector(selectMissions)
+  const mission = useAppSelector(state => selectMission(state, mockMissionId1))
+  const dispatch = useAppDispatch()
+
+  return (
+    <>
+      <h1>Relay 42</h1>
+      <BtnPrimary />
+      {missions.map(mission => (
+        <p key={mission.id}>{mission.destination}</p>
+      ))}
+      <button onClick={() => dispatch(addMission(mockMission))}>
+        Add Mission
+      </button>
+      <button
+        onClick={() =>
+          dispatch(editMission({ ...mockMission, destination: "Earth" }))
+        }
+      >
+        Edit Mission
+      </button>
+      Mission: {mission?.destination}
+    </>
+  )
 }
 export default Missions
